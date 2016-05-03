c05953ce206dfdc2e00310784cbe9f851353995a
Valmir Memeti <valmirmemeti@gmail.com>
2016-05-03 12:52:51 +0200

    Add twitter login to store session and login. Fix appropriate links showing up based on login state

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414134301) do
+ActiveRecord::Schema.define(version: 20160503095125) do
 
   create_table "lotteries", force: :cascade do |t|
     t.integer  "lottery_id"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160414134301) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
