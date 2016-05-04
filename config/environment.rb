4dc3aa76483936b172da23e8f85d0518c15d536b
OlavOlsm <OlavOlsm@gmail.com>
2016-05-04 16:01:58 +0200

    setup temporary ActionMailer config for email testing

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# show delivery errors
+ActionMailer::Base.raise_delivery_errors = true
+
+ActionMailer::Base.smtp_settings = {
+    :address        => 'cpanel.immortaltools.com',
+    :domain         => 'ghost.mxroute.com',
+    :ssl            => true,
+    :port           => '465',
+    :user_name      => 'bitcoinlottery@immortaltools.com',
+    :password       => 'SuchV3ryL0ttery',
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
