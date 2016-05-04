44d88b220c92960936026ba7903555985661cf0d
OlavOlsm <OlavOlsm@gmail.com>
2016-05-04 20:42:57 +0200

    Send confirmation email when user submits contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,6 +9,7 @@ class ContactsController < ApplicationController
     if @contact.deliver
       flash.now[:error] = nil
       flash.now[:notice] = 'Thank you. We will respond as soon as possible.'
+      ContactMailer.contact_confirmation(@contact).deliver_now
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "bitcoinlottery@immortaltools.com"
+  layout 'mailer'
+end
