295a7692960097191d257bcd94742d992abd594d
OlavOlsm <OlavOlsm@gmail.com>
2016-05-06 22:09:25 +0200

    Add method update_prize in Lottery; gets balance from btc address in block.io

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -1,2 +1,8 @@
 class Lottery < ActiveRecord::Base
+
+  def update_prize
+    result = BlockIo.get_address_balance :addresses => bitcoin_address
+    self.prize_amount = result['data']['available_balance']
+  end
+
 end
