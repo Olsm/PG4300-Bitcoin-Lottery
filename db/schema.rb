69ced2c42dc0f058cff552be8ec54b5343332004
OlavOlsm <OlavOlsm@gmail.com>
2016-05-09 22:23:16 +0200

    Add ends_at to Lottery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509153147) do
+ActiveRecord::Schema.define(version: 20160509200152) do
 
   create_table "lotteries", force: :cascade do |t|
     t.decimal  "prize_amount"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160509153147) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "winner_entry"
+    t.datetime "ends_at",         null: false
   end
 
   create_table "lottery_entries", force: :cascade do |t|
