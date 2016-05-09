e7a0b421d30e1bcccff7ae3df40ae2eaa6dc54bb
OlavOlsm <OlavOlsm@gmail.com>
2016-05-09 17:42:17 +0200

    Add pick_winner to Lottery using picker for weight based random winner

--- app/models/lottery.rb
+++ app/models/lottery.rb
@@ -6,7 +6,21 @@ class Lottery < ActiveRecord::Base
     result = BlockIo.get_address_balance :addresses => bitcoin_address
     self.prize_amount = result['data']['available_balance']
     self.save
+  end
 
+  def end
+    self.winner_entry = pick_winner
+    #TODO: Send prize to winner
+  end
+
+  private
+
+  def pick_winner
+    key_func = Proc.new{ |item| item.id }
+    weight_func = Proc.new{ |item| item.amount_charged }
+
+    pickup = Pickup.new(entries, key_func: key_func, weight_func: weight_func)
+    pickup.pick
   end
 
 end
