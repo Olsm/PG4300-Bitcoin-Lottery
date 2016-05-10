65e6b39b26c52cc90ebeab258a2fc01441a38709
Valmir Memeti <valmirmemeti@gmail.com>
2016-05-10 17:53:44 +0200

    add rails_admin and configure it for admin requirements

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass'
 gem 'autoprefixer-rails'
+gem 'rails_admin'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: {omniauth_callbacks: "omniauth_callbacks"}
   resources :lotteries
   resources :lottery_entries
@@ -12,7 +13,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'lotteries#index'
+  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
