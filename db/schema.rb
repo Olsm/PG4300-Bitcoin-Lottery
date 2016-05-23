c50e0fa66fa707c881c10c5ba1fb621f800cc780
Valmir Memeti <valmirmemeti@gmail.com>
2016-05-23 21:24:45 +0200

    add status and qr_code fields to lotteries

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516201313) do
+ActiveRecord::Schema.define(version: 20160523190614) do
 
   create_table "lotteries", force: :cascade do |t|
     t.decimal  "prize_amount",    default: 0.0, null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20160516201313) do
     t.integer  "winner_entry"
     t.datetime "ends_at",                       null: false
     t.string   "transaction_id"
+    t.string   "status"
+    t.string   "qr_code"
   end
 
   create_table "lotteries_lottery_fees", id: false, force: :cascade do |t|
