3fd99ce888df5a9c088e1d5a71545c19ce409be1
Valmir Memeti <valmirmemeti@gmail.com>
2016-06-05 19:18:24 +0200

    default test methods for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Called before every test method runs. Can be used
+  # to set up fixture information.
+  def setup
+    # Do nothing
+  end
+
+  # Called after every test method runs. Can be used to tear
+  # down fixture information.
+
+  def teardown
+    # Do nothing
+  end
+
+  # Fake test
+  def test_fail
+
+    fail('Not implemented')
+  end
 end
